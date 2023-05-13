dcdcc5dfd9c915fe10f66a0dbce0f5a094d50d71
nuublx <zeyaddiaa21@gmail.com>
2023-05-14 01:36:00 +0300

    README.md

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Dominoz
+
 Uniformed Search and informed Search using prolog to place the dominos on the board without touch the bombs
+
+installation:
+1-Install Node.js.
+2-Clone Dominoz to your computer.
+3-Run npm install.
+4-To start the development server, run npm start.
+
+Created Using:
+NodeJS
+ExpressJS
+Swipl
+Prolog
+HTML
+CSS
+Javascript
+
+Authors:
+Zeyad Diaa-Eldin:zeyaddiaa21@gmail.com
+Yahia ElHadidy: hadidiyehia@gmail.com
--- public/js/informed.js
+++ public/js/informed.js
@@ -3,54 +3,53 @@
 // for uninformed.ejs
 
 function sendBoard() {
-    var arr = makeArray();
-    callInformedApi(arr);
+  var arr = makeArray();
+  callInformedApi(arr);
 }
 
 const callInformedApi = function (arr) {
-    fetch("/informed/solution", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(arr),
-    }).then(async (response) => {
-        if (!response.ok) {
-            throw new Error("Network response was not ok");
-        }
-        const data = await response.json();
-        console.log(data);
-        getSolutions(data);
-    });
+  fetch("/informed/solution", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(arr),
+  }).then(async (response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+    console.log(data);
+    getSolutions(data);
+  });
 };
 
 const getSolutions = function (data) {
-    debugger;
-    const boards = [];
-    for (let sol of data) {
-        let [solution, goal] = parseStringToArray(sol);
-        boards.push(solution);
-    }
-    makeSolutionsBoards(boards)
+  debugger;
+  const boards = [];
+  for (let sol of data) {
+    let [solution, goal] = parseStringToArray(sol);
+    boards.push(solution);
+  }
+  makeSolutionsBoards(boards);
 };
 const parseStringToArray = function (str) {
-    let arr = [];
-    let row = [];
-    let goal = -1;
-    for (let i = 2; i < str.length - 1; i++) {
-        // push row in board
-        if (str[i] == "]" && row) {
-            arr.push(row);
-            row = [];
-        }
-        // row
-        if (str[i] != "[" && str[i] != "]" && str[i] != "," && str[i] != " ") {
-            if (str[i] === "*") row.push("f");
-            else row.push(str[i]);
-        }
-        // goal
-        if (str[i] == "]" && str[i - 1] == "]") goal = Number(str.slice(i + 1));
+  let arr = [];
+  let row = [];
+  let goal = -1;
+  for (let i = 2; i < str.length - 1; i++) {
+    // push row in board
+    if (str[i] == "]" && row) {
+      arr.push(row);
+      row = [];
     }
-    return [arr, goal];
+    // row
+    if (str[i] != "[" && str[i] != "]" && str[i] != "," && str[i] != " ") {
+      if (str[i] === "*") row.push("f");
+      else row.push(str[i]);
+    }
+    // goal
+    if (str[i] == "]" && str[i - 1] == "]") goal = Number(str.slice(i + 1));
+  }
+  return [arr, goal];
 };
-
